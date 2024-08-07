70b5179c40a9e94d6e352f43ba2fd77a0c33d61d
rza-wtag <rafi.zaman@welldev.io>
2024-08-07 09:22:42 +0600

    refactor

--- src/store/store.js
+++ src/store/store.js
@@ -14,36 +14,7 @@ export const fetchBoardData = createAsyncThunk(
     }
     return data.map((list) => ({
       ...list,
-      cards: list.cards || [],
     }));
-  }
-);
-
-export const addCardToSupabase = createAsyncThunk(
-  "board/addCardToSupabase",
-  async ({ listId, cardText }) => {
-    const { data: currentList, error: fetchError } = await supabase
-      .from("board")
-      .select("cards")
-      .eq("id", listId)
-      .single();
-
-    if (fetchError) {
-      throw new Error(fetchError.message);
-    }
-
-    const newCard = { id: Date.now(), title: cardText };
-    const updatedCards = [...currentList.cards, newCard];
-    const { data, error } = await supabase
-      .from("board")
-      .update({ cards: updatedCards })
-      .eq("id", listId);
-
-    if (error) {
-      throw new Error(error.message);
-    }
-
-    return { listId, newCard };
   }
 );
 
@@ -67,18 +38,9 @@ const boardSlice = createSlice({
       .addCase(fetchBoardData.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
-      })
-      .addCase(addCardToSupabase.fulfilled, (state, action) => {
-        const { listId, newCard } = action.payload;
-        const list = state.lists.find((list) => list.id === listId);
-        if (list) {
-          list.cards.push(newCard);
-        }
       });
   },
 });
-
-export const { addCard } = boardSlice.actions;
 
 const store = configureStore({
   reducer: {
